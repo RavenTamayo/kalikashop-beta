27eada58c69d10f80f673e34cd5d398f71d1129f
Raven Tamayo <mrjztamayo@tip.edu.ph>
2022-09-03 15:54:16 +0800

    added new container for what we do, and fixed the positioning

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,31 @@
 <template>
   <v-container fluid id="container">
     <v-row class="mt-16 mb-16">
-        <v-col></v-col>
+      <v-spacer></v-spacer>
         <v-col>
           <div class="text-body-2 text-green font-weight-black">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
           <div class="text-white text-h1">Welcome to KalikaShop</div>
           <div class="text-caption text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima iure quos adipisci itaque? Et, incidunt! Animi error numquam quae omnis!</div>
           <v-btn rounded="0" color="green-lighten-1 text-white mt-4" href="/store">Shop Now</v-btn></v-col>
-        <v-col></v-col>
+          <v-spacer></v-spacer>
     </v-row>
         
   </v-container>
   <v-container fluid>
     <v-row>
+      <v-spacer></v-spacer>
       <v-col>
         <div class="text-h3 text-center mt-5" fill-height>What We Do</div>
         <div class="text-body-1 text-center mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea eveniet velit ratione voluptatibus similique quam recusandae at magni. Illum vel dolorem dolor ipsum sint. Illo minima perferendis corporis est doloribus ipsum veniam numquam porro? Debitis quod explicabo dolore in autem velit quam sunt. Delectus explicabo tenetur velit quod nisi dolorem inventore pariatur neque. Sed laudantium voluptate labore tempore quisquam obcaecati ut deleniti illo, quo quia distinctio debitis autem molestias est beatae. Voluptate quia placeat officiis delectus eius provident quos cumque?</div>
       </v-col>
-      <v-col>
-        <v-img
-          src="src/images/plant.webp"
-          width="500"
-        ></v-img>
+      <v-spacer></v-spacer>
+    </v-row>
+    <v-row justify="center">
+    <v-spacer></v-spacer>
+      <v-col cols="auto">
+        <v-btn variant="outlined">Learn More</v-btn>
       </v-col>
+      <v-spacer></v-spacer>
     </v-row>
   </v-container>
 </template>
@@ -36,7 +39,7 @@
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-  height:50%;
+  height:60%;
    
 }
 </style>
